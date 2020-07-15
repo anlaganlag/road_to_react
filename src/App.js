4cf8e533cfd18c54872f0476deb9730d06b876c9
gal <anlaganlag>
2020-07-15 16:50:14 +0800

    用props.list傳遞數據

--- src/App.js
+++ src/App.js
@@ -1,54 +1,51 @@
 import React from 'react'
 
-const list = [
-  {
-    title: 'React',
-    url: 'https://reactjs.org/',
-    author: 'Jordan Walke',
-    num_comments: 3,
-    points: 4,
-    objectID: 0,
-  },
-  {
-    title: 'Redux',
-    url: 'https://redux.js.org/',
-    author: 'Dan Abramov, Andrew Clark',
-    num_comments: 2,
-    points: 5,
-    objectID: 1,
-  },
-];
-
-
-const App =() => {
+const App = () => {
+  const stories = [
+    {
+      title: 'React',
+      url: 'https://reactjs.org/',
+      author: 'Jordan Walke',
+      num_comments: 3,
+      points: 4,
+      objectID: 0,
+    },
+    {
+      title: 'Redux',
+      url: 'https://redux.js.org/',
+      author: 'Dan Abramov, Andrew Clark',
+      num_comments: 2,
+      points: 5,
+      objectID: 1,
+    },
+  ];
   const handleChange = e => {
-    console.log(e);
-  }
+    console.log(e.target.value);
+  };
   return (
   <div>
     <h1>Gal Stories</h1>
     <label htmlFor="search">Search: </label>
     <input id="search" type="text" onChange={handleChange} />
     <hr />
-    <List />
+    <List list={stories} />
   </div>
-  )
-}
+  );
+};
   
 
 
 
-const List= ()=> 
-    <div>
-      {list.map(e => 
-          <div key={e.objectID}>
-            <span><a href={e.url}>{e.title}</a></span>
-            <span>{e.author}</span>
-            <span>{e.num_comments}</span>
-            <span>{e.points}</span>
-          </div>
-      )}
+const List= props => 
+  props.list.map(e => (
+    <div key={e.objectID}>
+      <span><a href={e.url}>{e.title}</a></span>
+      <span>{e.author}</span>
+      <span>{e.num_comments}</span>
+      <span>{e.points}</span>
     </div>
+  ));
+
   
 export default App
   
